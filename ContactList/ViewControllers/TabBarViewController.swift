cb649ad29bd527900f099233e49d9f973c3542b2
Roman Khilchenko <extrimelife@hotmail.com>
2022-09-01 00:41:31 +0300

    HW Ready

--- ContactList/ViewControllers/TabBarViewController.swift
+++ ContactList/ViewControllers/TabBarViewController.swift
@@ -13,7 +13,6 @@ class TabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupTableViewControllers(personModel)
     }
     
